b70d7f13cf25ad62781c0ce20c8308dd9f7c60cb
MANUEL <rooarii.manuel@gmail.com>
2020-03-25 22:02:59 +0100

    Premier commit du site www.rooariimanuel.com

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "Nouveau message du site 'rooarii-manuel.com'"; //letter subject
+	$to = 'rooarii.manuel@gmail.com'; // Enter Your E-mail
+	$from = 'admin@you-site.com'; // Admin e-mail
+	$fromName = ''; // Your company name
+	$charset = 'UTF-8';
+?>
